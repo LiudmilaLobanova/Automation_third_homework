1571dc237ab28dc9f5614af82c2d4463a86fe18a
LiudmilaLobanova <liudmilanl@gmail.com>
2023-03-11 00:37:15 -0800

    Headless added

--- src/test/java/ru/netology/newcard/NewCardTest.java
+++ src/test/java/ru/netology/newcard/NewCardTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class NewCardTest {
 
@@ -15,7 +16,13 @@ public class NewCardTest {
     }
 
     @BeforeEach
-    void setUp() {driver = new ChromeDriver();
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+//        driver = new ChromeDriver();
     }
 
     @AfterEach
